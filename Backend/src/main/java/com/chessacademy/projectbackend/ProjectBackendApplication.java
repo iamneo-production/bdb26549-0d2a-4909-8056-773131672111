335c6b58b58159f313abbb73208d42a3de732480
Madhusudan Anand <54022235+MadhusudanAnand@users.noreply.github.com>
2022-04-01 23:52:02 +0530

    Update ProjectBackendApplication.java

--- Backend/src/main/java/com/chessacademy/projectbackend/ProjectBackendApplication.java
+++ Backend/src/main/java/com/chessacademy/projectbackend/ProjectBackendApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+
 @EnableAutoConfiguration(exclude = { ErrorMvcAutoConfiguration.class })
+
 public class ProjectBackendApplication {
 
 	public static void main(String[] args) {
