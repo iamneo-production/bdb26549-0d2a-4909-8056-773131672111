5e01eac7b71bd63fe03947bebf41dcda3503873a
Madhusudan Anand <54022235+MadhusudanAnand@users.noreply.github.com>
2022-04-01 23:51:16 +0530

    Update CourseRepository.java

--- Backend/src/main/java/com/chessacademy/projectbackend/Repository/CourseRepository.java
+++ Backend/src/main/java/com/chessacademy/projectbackend/Repository/CourseRepository.java
@@ -6,6 +6,7 @@ import com.chessacademy.projectbackend.Models.CourseModel;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 
 public interface CourseRepository extends JpaRepository<CourseModel,Long> {
 }
