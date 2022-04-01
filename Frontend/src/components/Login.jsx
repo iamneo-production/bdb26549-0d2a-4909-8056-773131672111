51fb89e21f8caa5fa37906e489923a57286d80fc
Madhusudan Anand <54022235+MadhusudanAnand@users.noreply.github.com>
2022-04-02 00:19:55 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,59 +1,128 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import axios from "axios";
-import "./Styles.css";
-import authHeader from "./auth-header";
+import { Link } from 'react-router-dom';
+
+import "./Style.css";
 
 export class Login extends Component {
+    constructor() {
+        super()
 
-  constructor() {
-    super();
+        this.state = {
+            email: "",
+            password: "",
+            username: "",
+            mobileNumber:"",
+            confirmPassword: "",
+            role:[""],
+            errors: {
+                email: "",
+                
+                password:"",
 
-    this.state = {
-      email: "",
-      password: "",
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  onChangeHandler = (event) => {
-    this.setState({
-        [event.target.name]: event.target.value
-    })
-}
-
-
-  handleSubmit = (event) => {
-    if (this.state.password.toString().length <= 8) {
-      alert(`Password must contain atleast 8 characters.`);
-    } else if (
-      this.state.password.search(/[0-9]/) === -1 ||
-      this.state.password.search(/[a-z]/) === -1 ||
-      this.state.password.search(/[A-Z]/) === -1 ||
-      this.state.password.search(
-        //eslint-disable-next-line
-        /[!\@\#\$\^\&\*\(\)\+\=\-\/\?\.\,\>\<\}\{\]\[\'\"\;\:\]\}\{\`\~]/
-      ) === -1
-    ) {
-      alert(
-        `Password must contain atleast 1 number, 1 Uppercase, 1 Lowercase and 1 Special character.`
-      );
-    } else {
-      console.log(this.state);
-      this.setState({
-        email: "",
-        password: "",
-      });
+              }       
+        }
+        this.handleSubmit=this.handleSubmit.bind(this)
     }
+    isEmailValid = email => {
+        //eslint-disable-next-line
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{3,}))$/;
+        return re.test(String(email).toLowerCase());
+      };
+    
+    
+    validateEmail = email => {
+        const errors = {
+          email: "",
+        };
+    
+        if (!this.isEmailValid(email)) {
+          errors.email = "Email is invalid";
+        }
 
-    event.preventDefault();
-
+        this.setState(state => ({ ...state, errors }));
+    };
+    
+    
+     validatePassword= password =>{
+         const errors={
+             password:""
+         };
+         if(this.state.password.toString().length <6){
+             errors.password="Password must contain atleast 6 characters."
+         }
+         //eslint-disable-next-line
+         else if(this.state.password.search(/[0-9]/)===0 || this.state.password.search(/[a-z]/)===-1 ||
+                    this.state.password.search(/[A-Z]/)===-1 || 
+                    //eslint-disable-next-line
+                    this.state.password.search(/[!\@\#\$\^\&\*\(\)\+\=\-\/\?\.\,\>\<\}\{\]\[\'\"\;\:\]\}\{\`\~]/)===-1 )
+                    {   
+            errors.password="Password must contain atleast 1 number, 1 Uppercase, 1 Lowercase & 1 Special character."
+        }
+        
+        this.setState(state => ({ ...state, errors }));
+     }
+     
+     
+     
+      handleChange = ({ target: { name, value } }) => {
+        this.setState({ [name]: value });
+    
+        switch (name) {
+            case "email":
+                this.validateEmail(value);
+                break;
+            
+            case "password":
+              this.validatePassword(value);
+              break;
+            
+            default:
+            break;
+        }
+      };
     
 
+    onChangeHandler = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    // handleValidations=(event)=>{
+    //     if(this.state.password>0)
+    //         return true;
+    // }
+  
+    handleSubmit = (event) => {
 
-    const user = {
-      email: this.state.email,
-      password: this.state.password,
-    };
+        if(this.state.password.toString().length <6)
+        {
+            alert(`Password must contain atleast 6 characters and contain atleast 1 number, 1 Uppercase, 1 Lowercase & 1 Special character.`)
+        }
+        else if(this.state.password.search(/[0-9]/)===-1 || this.state.password.search(/[a-z]/)===-1 
+        || this.state.password.search(/[A-Z]/)===-1 || this.state.password.search(/[!\@\#\$\^\&\*\(\)\+\=\-\/\?\.\,\>\<\}\{\]\[\'\"\;\:\]\}\{\`\~]/)===-1 )
+        {
+            alert(`Password must contain atleast 6 characters and contain atleast 1 number, 1 Uppercase, 1 Lowercase & 1 Special character.`)
+        }
+        
+        else{
+            this.setState({
+                
+                email: "",
+                
+                password: "",
+                
+            })
+            alert(`Logged in`)
+        }
+        event.preventDefault();
+        
+        console.log(this.state);
+        const user = {
+            email: this.state.email,
+            
+            password: this.state.password,
+        }
     axios
         .post("http://localhost:8080/signin", user,{ headers: authHeader() })
         .then((response) => {
@@ -74,42 +143,46 @@ export class Login extends Component {
 
   
 
-  render() {
-    const {email,password}=this.state
-    return (
-      <div className="login">
-        <div className="head">
-          <h3 id="h">Login </h3>
+ render() {
+      const {email,password}=this.state
+      const isEnabled = this.isEmailValid(email) 
+                               
+
+    return(
+        <div className='m'>
+            <div className='container'>
+                <div className="Regox">
+                    <form className='Regform'onSubmit={this.handleSubmit}>
+                        <h1 align ="center" id='h1'>Login</h1>
+                        <p></p>
+                        <p></p>
+                        
+                        <div className="mb-3">
+                            <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                            <input type="email" value={email} name='email' onChange={this.handleChange} className="form-control" placeholder="Enter Email" id="email" autocomplete="off" required/>
+                            {this.state.errors.email && <span style={{color:"yellow"}}>{this.state.errors.email}</span>}
+                        </div>
+                        
+                        
+                        <div className="mb-3">
+                            <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                            <input type="password" value={password} name='password' onChange={this.handleChange} className="form-control" placeholder="Enter Password" id="password" autoComplete='off' required/>
+                            {this.state.errors.password && <span style={{color:"yellow"}}>{this.state.errors.password}</span>}
+
+                        </div>
+                        
+                        <button disabled={!isEnabled} type="submit" value="Submit" className="btn btn-primary">Register</button>
+                        
+                        <p id='h1'>New User/Admin? <Link to="/signup">Signup</Link></p>
+                        
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className="loginBox">
-          <form className="form" onSubmit={this.handleSubmit}>
-            <div className="mb-3">
-              <label htmlFor="exampleInputEmail1" className="form-label">
-                Email address
-              </label>
-              <input type="email" value={email} onChange={this.onChangeHandler} className="form-control"
-                      placeholder="Enter Email"  id="email" name="email" required />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="exampleInputPassword1" className="form-label">
-                Password
-              </label>
-              <input type="password" value={password} onChange={this.onChangeHandler} className="form-control"
-                placeholder="Enter Password" id="password" name="password" required/>
-            </div>
-            <div className="loginbtn">
-              <button type="submit" value="Submit" className="btn btn-primary">
-                Login
-              </button>
-              <p>
-                New User/Admin ? <a href="signup">Sign up</a>
-              </p>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
+        )
+    }
+}
+
 }
 
 
