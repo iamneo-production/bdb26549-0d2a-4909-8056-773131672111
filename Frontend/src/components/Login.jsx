4a8eb424f6aff9860d144fd5dfe7d0518fa366d5
Madhusudan Anand <54022235+MadhusudanAnand@users.noreply.github.com>
2022-04-02 00:25:03 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from "axios";
 import { Link } from 'react-router-dom';
 
-import "./Style.css";
+import "./Styles.css";
 
 export class Login extends Component {
     constructor() {
